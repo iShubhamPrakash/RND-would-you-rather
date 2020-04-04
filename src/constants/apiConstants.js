b1b49c123fadf6f7b3af0fcf657b4530504eadd0
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-04-04 15:54:10 +0530

    Add API constants

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,12 +1,6 @@
 import {_getUsers,_getQuestions,_saveQuestion,_saveQuestionAnswer} from '../api/_DATA'
 
-/*
-_getUsers()
-
-_getQuestions()
-
-_saveQuestion (question)
-
-_saveQuestionAnswer ({ authedUser, qid, answer })
-
-*/
+export const getUsers=()=>_getUsers()
+export const getQuestion=()=> _getQuestions()
+export const saveQuestion=(question)=> _saveQuestion(question)
+export const saveQuestionAnswer=({ authedUser, qid, answer })=> _saveQuestionAnswer({ authedUser, qid, answer })
